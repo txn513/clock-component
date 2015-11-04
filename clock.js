d7f626de0a8ab104e245c04b66600bf62cb29503
txn513 <xtian513@gmail.com>
2015-11-05 03:54:16 +0800

    add prototype function

--- clock.js
+++ clock.js
@@ -1,70 +1,81 @@
-function Clock(){
-	this.oHour = document.getElementById('hour');
-	this.oMin = document.getElementById('min');
-	this.oSec = document.getElementById('sec');
-	this.oUl = document.getElementById('list');
-	this.oCss = document.getElementById('css');
-	this.oZone = document.getElementById('zone-list')
-	this.myDate = null;
-	this.myHour = null;
-	this.myMin = null;
-	this.mySec = null;
-	this.cssContent = null;
-	this.setting = {
-		hourColor: '#000',
-		minColor: '#888',
-		secColor: 'red'
-	};
-}
-Clock.prototype.init = function(obj){
-	var This = this;
-	this.extend(this.setting, obj)
-	for(var i=0; i<60; i++){
-		this.oUl.innerHTML += "<li></li>";
-		this.cssContent += "#body li:nth-of-type("+ (i+1) 
-		+"){transform: rotate("+ (i*6) +"deg);}"
+var Clock = (function(){
+	function Clock(){
+		this.oHour = document.getElementById('hour');
+		this.oMin = document.getElementById('min');
+		this.oSec = document.getElementById('sec');
+		this.oUl = document.getElementById('list');
+		this.oCss = document.getElementById('css');
+		this.oZone = document.getElementById('zone-list');
+		this.oBtn = document.getElementById('btn');
+		this.newTime = null;
+		this.myDate = null;
+		this.myHour = null;
+		this.myMin = null;
+		this.mySec = null;
+		this.cssContent = null;
+		this.setting = {
+			hourColor: '#000',
+			minColor: '#888',
+			secColor: 'red',
+			offset: '8'
+		};
 	}
-	this.oCss.innerHTML += this.cssContent;
-	setInterval(function(){
-		This.getTime();
-	}, 1000);
-	this.setColor();
-	this.creatTimeZone();
-}
-Clock.prototype.getTime = function(){
-	this.myDate = new Date();
-	this.mySec = this.myDate.getSeconds();
-	this.myMin = this.myDate.getMinutes() + this.mySec/60;
-	this.myHour = this.myDate.getHours() + this.myMin/60;
-	
-	this.oHour.style.transform =  "rotate("+ (this.myHour*30) +"deg)";
-	this.oMin.style.transform =  "rotate("+ (this.myMin*6) +"deg)";
-	this.oSec.style.transform = "rotate("+ (this.mySec*6) +"deg)";
-}
-Clock.prototype.setColor = function(){
-	this.oHour.style.backgroundColor =  this.setting.hourColor;
-	this.oMin.style.backgroundColor =  this.setting.minColor;
-	this.oSec.style.backgroundColor = this.setting.scColor;
-}
-Clock.prototype.extend = function(obj1, obj2){
-	for(var attr in obj2){
-		obj1[attr] = obj2[attr];
-	};
-}
-Clock.prototype.creatTimeZone = function(){
-	var This = this;
-	var xhr = new XMLHttpRequest();
-	var html = null;
-	xhr.open('get','./timezones.json', true);
-	xhr.send();
-	xhr.onreadystatechange = function(){
-		if(xhr.readyState == 4){
-			var data = JSON.parse(xhr.responseText);
-			console.log(Object.prototype.toString.call(data));
-			for (var i=0; i<data.length; i++){
-				html += "<option index='"+ i +"'>"+ data[i].text +"</option>";
+	Clock.prototype.init = function(obj){
+		var This = this;
+		this.extend(this.setting, obj)
+		for(var i=0; i<60; i++){
+			this.oUl.innerHTML += "<li></li>";
+			this.cssContent += "#body li:nth-of-type("+ (i+1) 
+			+"){transform: rotate("+ (i*6) +"deg);}"
+		}
+		this.oCss.innerHTML += this.cssContent;
+		setInterval(function(){
+			This.getTime();
+		}, 1000);
+	}
+	Clock.prototype.getTime = function(){
+		this.myDate = new Date();
+		this.mySec = this.myDate.getSeconds();
+		this.myMin = this.myDate.getMinutes() + this.mySec/60;
+		this.myHour = this.myDate.getHours() + this.myMin/60
+		this.newTime = this.myHour - (8 - this.setting.offset);
+
+		this.oHour.style.transform =  "rotate("+ (this.newTime*30) +"deg)";
+		this.oMin.style.transform =  "rotate("+ (this.myMin*6) +"deg)";
+		this.oSec.style.transform = "rotate("+ (this.mySec*6) +"deg)";
+	}
+	Clock.prototype.setColor = function(){
+		this.oHour.style.backgroundColor =  this.setting.hourColor;
+		this.oMin.style.backgroundColor =  this.setting.minColor;
+		this.oSec.style.backgroundColor = this.setting.scColor;
+	}
+	Clock.prototype.extend = function(obj1, obj2){
+		for(var attr in obj2){
+			obj1[attr] = obj2[attr];
+		};
+	}
+	Clock.prototype.getZone = function(){
+		var This = this;
+		var xhr = new XMLHttpRequest();
+		var html = null;
+		var index = null;
+		var offset = null;
+		xhr.open('get','./timezones.json', true);
+		xhr.send();
+		xhr.onreadystatechange = function(){
+			if(xhr.readyState == 4){
+				var data = JSON.parse(xhr.responseText);
+				for (var i=0; i<data.length; i++){
+					html += "<option zone-index='"+ i +"'>"+ data[i].text +"</option>";
+				}
+				This.oZone.innerHTML = html;
+				This.oBtn.onclick = function(){
+		 			index = This.oZone.selectedIndex;
+		 			offset = data[index].offset;
+		 			This.setting.offset = offset;
+		 		}
 			}
-			This.oZone.innerHTML = html;
 		}
 	}
-}
+	return Clock;
+})();
